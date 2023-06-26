b6a323fbea3abc8b21b26a91b2642f483bc9afdd
SoldatdOR05 <epaleyron@edenschool.fr>
2023-06-26 17:16:26 +0200

    animate h1 pad fini

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// Hello World
+
+
+// Scroll nav
+const header = document.querySelector("header");
+let scrollValue = 0;
+
+document.addEventListener("scroll", () => {
+    let top  = document.documentElement.scrollTop;
+    if (scrollValue < top) {
+        header.classList.add("hidden");
+    } else {
+        header.classList.remove("hidden");
+    }
+    scrollValue = top;
+})
+
+// IntersectionObserver
 const observer = new IntersectionObserver((entries) => {
     for(const entry of entries) {
         if (entry.isIntersecting) {
@@ -5,13 +23,14 @@ const observer = new IntersectionObserver((entries) => {
                 {transform: "transcaleX(-100px)", opacity: 0},
                 {transform: "transcaleX(0px)", opacity: 1},
             ], {
-                duration: 2000
+                duration: 2000,
             })
         }
     }
 }, {
-    rootMargin: "50px 50px 50px 50px"
+    rootMargin: "50px 50px 50px 50px",
 })
 
-observer.observe(document.querySelector(".scrollspy1"));
-observer.observe(document.querySelector(".scrollspy2"));
+observer.observe(document.querySelector(".scrollspy"));
+const section = document.querySelectorAll("section");
+observer.observe = document.querySelectorAll("a");
